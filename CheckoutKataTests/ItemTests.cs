7e8bbfa85b41c52eda2f0edcb5d82add209d5fca
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 21:43:01 +0000

    added test for increaseStock method

--- CheckoutKataTests/ItemTests.cs
+++ CheckoutKataTests/ItemTests.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using CheckoutKataNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CheckoutKataTests
 {
     class ItemTests
     {
+        [TestClass]
+        public class BasketTests
+        {
+            [TestMethod]
+            public void IncreaseStock_AddOneItem_CorrectStock()
+            {
+                // Arrange
+                int expected = 1;
+                Item item = new Item(0, 10, "none");
+
+                // Act
+                item.IncreaseStock();
+
+                // Assert
+                int actual = item.Amount;
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
     }
 }

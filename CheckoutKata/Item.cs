b34ccf2b957961d94cfd356a16086b0b3fe9e222
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 20:36:03 +0000

    setup files and Item class

---
+++ CheckoutKata/Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKataNS
+{
+    class Item
+    {
+        private int _amount;
+        private double _unitPrice;
+
+        public Item(int amount, double unitPrice)
+        {
+            _amount = amount;
+            _unitPrice = unitPrice;
+        }
+
+        public int Amount
+        {
+            get { return _amount; }
+        }
+
+        public double UnitPrice
+        {
+            get { return _unitPrice; }
+        }
+    }
+}
---
+++ CheckoutKataTests/BasketTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckoutKataTests
+{
+    class BasketTests
+    {
+    }
+}

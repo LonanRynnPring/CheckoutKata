f4da52ea221eeb027b4a3a807a9fdd0c29641d41
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 20:18:19 +0000

    added test project

---
+++ CheckoutKataTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutKataNS;
+
+namespace CheckoutKataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

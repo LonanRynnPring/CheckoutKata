3fdbbb65061072341bc9dd4df76e47f94686219d
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 21:37:43 +0000

    added more tests

--- CheckoutKataTests/BasketTests.cs
+++ CheckoutKataTests/BasketTests.cs
@@ -117,5 +117,54 @@ namespace CheckoutKataTests
             double actual = checkout.basketTotal;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void AddItem_AddAllItems_CorrectPrice()
+        {
+            // Arrange
+            double expected = 120;
+            Basket checkout = new Basket();
+            Item A = new Item(0, 10, "none");
+            Item B = new Item(0, 15, "3 for 40");
+            Item C = new Item(0, 40, "none");
+            Item D = new Item(0, 55, "25% off for every 2 purchased together");
+            Item[] basket = { A, B, C, D };
+
+            // Act
+            checkout.AddItem(A, basket);
+            checkout.AddItem(B, basket);
+            checkout.AddItem(C, basket);
+            checkout.AddItem(D, basket);
+
+            // Assert
+            double actual = checkout.basketTotal;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddItem_AddAllItemsWithDiscounts_CorrectPrice()
+        {
+            // Arrange
+            double expected = 172.5;
+            Basket checkout = new Basket();
+            Item A = new Item(0, 10, "none");
+            Item B = new Item(0, 15, "3 for 40");
+            Item C = new Item(0, 40, "none");
+            Item D = new Item(0, 55, "25% off for every 2 purchased together");
+            Item[] basket = { A, B, C, D };
+
+            // Act
+            checkout.AddItem(A, basket);
+            checkout.AddItem(B, basket);
+            checkout.AddItem(B, basket);
+            checkout.AddItem(B, basket);
+            checkout.AddItem(C, basket);
+            checkout.AddItem(D, basket);
+            checkout.AddItem(D, basket);
+
+            // Assert
+            double actual = checkout.basketTotal;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }

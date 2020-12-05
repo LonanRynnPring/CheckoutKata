510f739dfe3e01e66e7270541d04310a802eb891
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 20:59:11 +0000

    updated AddItem Method and created CalculateBasketTotal method

--- CheckoutKata/Basket.cs
+++ CheckoutKata/Basket.cs
@@ -8,10 +8,21 @@ namespace CheckoutKataNS
     {
         public double basketTotal;
 
-        public void AddItem(Item SKU, int[] basket)
+        public void AddItem(Item SKU, Item[] basket)
         {
             SKU.IncreaseStock();
             // calculate total
+        }
+
+        public void CalculateBasketTotal(Item[] basket)
+        {
+            double total = 0;
+            basketTotal = 0;
+            foreach (Item item in basket)
+            {
+                total += item.Cost();
+            }
+            basketTotal += total;
         }
     }
 }

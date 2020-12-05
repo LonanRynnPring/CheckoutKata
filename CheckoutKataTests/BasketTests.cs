3dca26a0020e9f096d774f81479d77c5950f433f
lonan rynn-pring <lonanrynnpring@gmail.com>
2020-12-05 21:12:43 +0000

    created test for discount for B

--- CheckoutKataTests/BasketTests.cs
+++ CheckoutKataTests/BasketTests.cs
@@ -28,5 +28,27 @@ namespace CheckoutKataTests
             double actual = checkout.basketTotal;
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void AddItem_AddThreeItemB_CorrectPrice()
+        {
+            // Arrange
+            double expected = 40;
+            Basket checkout = new Basket();
+            Item A = new Item(0, 10);
+            Item B = new Item(0, 15);
+            Item C = new Item(0, 40);
+            Item D = new Item(0, 55);
+            Item[] basket = { A, B, C, D };
+
+            // Act
+            checkout.AddItem(B, basket);
+            checkout.AddItem(B, basket);
+            checkout.AddItem(B, basket);
+
+            // Assert
+            double actual = checkout.basketTotal;
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
